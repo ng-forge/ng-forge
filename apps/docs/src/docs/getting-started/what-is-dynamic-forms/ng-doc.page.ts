37dfeea89143363cd69a74e0cd849e69d9b03aff
antimprisacaru <antimpris@gmail.com>
2025-11-05 17:26:01 +0200

    fix: add category to get started pages

--- apps/docs/src/docs/getting-started/what-is-dynamic-forms/ng-doc.page.ts
+++ apps/docs/src/docs/getting-started/what-is-dynamic-forms/ng-doc.page.ts
@@ -1,10 +1,12 @@
 import { NgDocPage } from '@ng-doc/core';
 import { QuickStartDemoComponent } from '../examples/quick-start-demo.component';
+import GettingStartedCategory from '../ng-doc.category';
 
 const WhatIsDynamicFormsPage: NgDocPage = {
   title: 'What is Dynamic Forms?',
   mdFile: './index.md',
   order: 0,
+  category: GettingStartedCategory,
   playgrounds: {
     DemoFormPlayground: {
       target: QuickStartDemoComponent,
