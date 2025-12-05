7f6096100cff23679d9ef831eda9456dde1925ad
antimprisacaru <antimpris@gmail.com>
2025-12-05 22:32:49 +0200

    small issue fixes

--- packages/dynamic-forms/src/lib/mappers/index.ts
+++ packages/dynamic-forms/src/lib/mappers/index.ts
@@ -6,3 +6,4 @@ export { rowFieldMapper } from './row/row-field-mapper';
 export { groupFieldMapper } from './group/group-field-mapper';
 export { arrayFieldMapper } from './array/array-field-mapper';
 export { pageFieldMapper } from './page/page-field-mapper';
+export { textFieldMapper } from './text/text-field-mapper';
