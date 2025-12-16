07269695cf79a7968e314d97b73e3d7f2f1a7e80
antimprisacaru <antimpris@gmail.com>
2025-12-16 21:53:09 +0200

    fix(dynamic-forms): use export type for type-only exports in logger module
    Fix TS1205 error when isolatedModules is enabled by using 'export type'
    for DynamicFormLogger interface and LoggerFeatureOptions type.

--- packages/dynamic-forms/src/lib/providers/features/logger/index.ts
+++ packages/dynamic-forms/src/lib/providers/features/logger/index.ts
@@ -1,6 +1,7 @@
 export { LogLevel } from './log-level';
-export { DynamicFormLogger } from './logger.interface';
+export type { DynamicFormLogger } from './logger.interface';
 export { DYNAMIC_FORM_LOGGER } from './logger.token';
 export { ConsoleLogger } from './console-logger';
 export { NoopLogger } from './noop-logger';
-export { withLogger, LoggerFeatureOptions } from './with-logger';
+export { withLogger } from './with-logger';
+export type { LoggerFeatureOptions } from './with-logger';
